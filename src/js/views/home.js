9dc4acf095d7e2ddcd54e01d726431f113892ef4
Ligia88 <129026864+Ligia88@users.noreply.github.com>
2023-08-19 18:43:29 +0000

    commit

--- src/js/views/home.js
+++ src/js/views/home.js
@@ -1,15 +1,46 @@
 import React from "react";
-import rigoImage from "../../img/rigo-baby.jpg";
 import "../../styles/home.css";
 
-export const Home = () => (
-	<div className="text-center mt-5">
-		<h1>Hello Rigo!</h1>
-		<p>
-			<img src={rigoImage} />
-		</p>
-		<a href="#" className="btn btn-success">
-			If you see this green button, bootstrap is working
-		</a>
-	</div>
-);
+export const Home = () => {
+	return (
+		<div className="container">
+			<h1 className="text-danger">Characters</h1>
+
+			{/* Carta original */}
+			<div className="my-carousel">
+				<div className="my-card">
+					<div>
+						<img src="" alt="Descripción de la imagen" />
+					</div>
+					<div>
+						<p>Gender</p>
+						<p>Hair Color</p>
+						<p>Eye Color</p>
+						<p>Cuarto párrafo</p>
+					</div>
+					<button className="btn btn-primary">Learn More</button>
+					<button>C</button>
+				</div>
+			</div>
+
+			{/* Clones de la carta */}
+			{Array.from({ length: 8 }).map((_, index) => (
+				<div key={index} className="border border-danger">
+					<div className="my-card">
+						<div>
+							<img src="" alt="Descripción de la imagen" />
+						</div>
+						<div>
+							<p>Gender</p>
+							<p>Hair Color</p>
+							<p>Eye Color</p>
+							<p>Cuarto párrafo</p>
+						</div>
+						<button className="btn btn-primary">Learn More</button>
+						<button>C</button>
+					</div>
+				</div>
+			))}
+		</div>
+	);
+};
