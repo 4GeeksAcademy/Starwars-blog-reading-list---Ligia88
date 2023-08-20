3af2715e583c25391340a46d25dd45c367aded75
Ligia88 <129026864+Ligia88@users.noreply.github.com>
2023-08-20 19:09:38 +0000

    commit

--- src/js/views/home.js
+++ src/js/views/home.js
@@ -4,46 +4,44 @@ import "../../styles/home.css";
 export const Home = () => {
 	return (
 		<div className="container">
-			<h1 className="text-danger">Characters</h1>
-
-			{/* Contenedor de cartas */}
+			{/* Contenedor de Characters */}
 			<div className="my-carousel d-flex overflow-x-scroll">
-				{/* Carta original */}
-				<div className="border border-danger my-card mx-2"> {/* Agregamos la clase mx-2 para agregar un margen horizontal entre las cartas */}
+				<div className="my-card">
+					<h1 className="text-danger my-title">Characters</h1>
 					<div>
 						<img src="https://picsum.photos/200/300" alt="Descripción de la imagen" />
 					</div>
-					<div className="">
-					        <p>Name</p>
-							<p>Gender</p>
-							<p>Hair Color</p>
-							<p>Eye Color</p>
+					<div className="body-text">
+						<p>Name</p>
+						<p>Gender</p>
+						<p>Hair Color</p>
+						<p>Eye Color</p>
 					</div>
 					<div className="button-footer">
-					<button className="btn btn-primary">Learn More</button>
-					<button className="btn btn-warning">C</button>
+						<button className="btn btn-primary">Learn More</button>
+						<button className="btn btn-warning">C</button>
 					</div>
 				</div>
-
 				{/* Clones de la carta */}
-				{Array.from({ length: 8 }).map((_, index) => (
-					<div key={index} className="border border-danger my-card mx-2"> {/* Agregamos la clase mx-2 para agregar un margen horizontal entre las cartas */}
-						<div>
+			</div>
+			
+			{/* Contenedor de Planets */}
+			<div className="my-carousel d-flex overflow-x-scroll">
+				<div className="my-card">
+					<h1 className="text-danger my-title">Planets</h1>
+					<div>
 						<img src="https://picsum.photos/200/300" alt="Descripción de la imagen" />
-						</div>
-						<div>
-						    <p>Name</p>
-							<p>Gender</p>
-							<p>Hair Color</p>
-							<p>Eye Color</p>
-						</div>
+					</div>
+					<div className="body-text">
+						<p>Population</p>
+						<p>Terraine</p>
+					</div>
 					<div className="button-footer">
-					<button className="btn btn-primary">Learn More</button>
-					<button className="btn btn-warning">C</button>
+						<button className="btn btn-primary">Learn More</button>
+						<button className="btn btn-warning">C</button>
 					</div>
-
-					</div>
-				))}
+				</div>
+				{/* Clones de la carta */}
 			</div>
 		</div>
 	);
