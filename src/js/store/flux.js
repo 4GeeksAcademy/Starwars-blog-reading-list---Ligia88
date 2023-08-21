e2b885f77cb1bcf33d93d9b4c67ea0125b76ea1b
Ligia88 <129026864+Ligia88@users.noreply.github.com>
2023-08-21 23:44:33 +0000

    commit

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -17,39 +17,28 @@ const getState = ({ getStore, getActions, setStore }) => {
 			]
 		},
 		actions: {
-			// Use getActions to call a function within a fuction
 			exampleFunction: () => {
 				getActions().changeColor(0, "green");
 			},
 			loadSomeData: () => {
-				/**
-					fetch().then().then(data => setStore({ "foo": data.bar }))
-				*/
+				// fetch().then().then(data => setStore({ "foo": data.bar }))
 			},
 			changeColor: (index, color) => {
-				//get the store
 				const store = getStore();
-
-				//we have to loop the entire demo array to look for the respective index
-				//and change its color
 				const demo = store.demo.map((elm, i) => {
 					if (i === index) elm.background = color;
 					return elm;
 				});
-
-				//reset the global store
 				setStore({ demo: demo });
 			},
-			get[Characters] () => {
+			getCharacters: () => {
 				fetch(`${getStore().urlBase}/people`)
 				.then(response => response.json())
-				.then((data) => {
-					for(let item of data){
-						console.log(item)
-					}
-				
-				
-		})
+				.then(data => {
+					
+				});
+			}
+		}
 	};
 };
 
