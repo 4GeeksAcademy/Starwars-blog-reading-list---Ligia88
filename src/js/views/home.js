ffbf8d85a7256d93c643e229fa2f1c209a8591d2
Ligia88 <129026864+Ligia88@users.noreply.github.com>
2023-09-04 05:29:31 +0000

    commit

--- src/js/views/home.js
+++ src/js/views/home.js
@@ -8,22 +8,26 @@ export const Home = () => {
 		<div className="container">
 			{/* Contenedor de Characters */}
 			<div className="my-carousel d-flex overflow-x-scroll">
-				<div className="my-card">
-					<h1 className="text-danger my-title">{Characters}</h1>
-					<div>
-						<img src="https://picsum.photos/200/300" alt="Descripción de la imagen" />
-					</div>
-					<div className="body-text">
-						<p>Name</p>
-						<p>Gender</p>
-						<p>Hair Color</p>
-						<p>Eye Color</p>
-					</div>
-					<div className="button-footer">
-						<button className="btn btn-primary">Learn More</button>
-						<button className="btn btn-warning">C</button>
-					</div>
-				</div>
+				{store.characters.map((item) =>{
+					return(
+						<div className="my-card">
+						<h1 className="text-danger my-title">{Characters}</h1>
+						<div>
+							<img src="https://picsum.photos/200/300" alt="Descripción de la imagen" />
+						</div>
+						<div className="body-text">
+							<p>Name</p>
+							<p>Gender</p>
+							<p>Hair Color</p>
+							<p>Eye Color</p>
+						</div>
+						<div className="button-footer">
+							<button className="btn btn-primary">Learn More</button>
+							<button className="btn btn-warning">C</button>
+						</div>
+				        </div>
+							);
+						})}
 				{/* Clones de la carta */}
 			</div>
 			
