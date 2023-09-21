c2f720f15221f7389c26df2fea48dff383659ae1
Ligia88 <129026864+Ligia88@users.noreply.github.com>
2023-09-21 00:04:40 +0000

    navbar

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -1,42 +1,39 @@
-
-import React,{useContext} from "react";
+import React, { useContext } from "react";
 import { Link } from "react-router-dom";
 import { Context } from "../store/appContext";
 
 export const Navbar = () => {
-	const {store,actions} = useContext(Context)
-	console.log (store.favorites)
-	return (
-		<nav className="navbar navbar-light bg-light mb-3">
-			<div className="container">
-				<Link to="/">
-					<span className="navbar-brand mb-0 h1">Star Wars</span>
-				</Link>
-				<div className="ml-auto">
-					<div className="dropdown">
-						<button
-							className="btn btn-primary dropdown-toggle"
-							type="button"
-							data-bs-toggle="dropdown"
-							aria-expanded="false"
-						>
-							{/* Favorites {store.favorite.length} */}
-						</button>
-						<ul className="dropdown-menu">
-							{/* {store.favorite.map((item)=> {
-								return <li>
-								<a className="dropdown-item" href="#">
-									{item.name}
-								</a>
-							</li>
-							})} */}
-							
-						
-						</ul>
-					</div>
-				</div>
-			</div>
-		</nav>
-	);
-};
+  const { store, actions } = useContext(Context);
+  console.log(store.favorites);
 
+  return (
+    <nav className="navbar navbar-dark bg-dark mb-3">
+      <div className="container">
+        <Link to="/">
+          <span className="navbar-brand mb-0 h1">Star Wars</span>
+        </Link>
+        <div className="ml-auto">
+          <div className="dropdown">
+            <button
+              className="btn btn-primary dropdown-toggle"
+              type="button"
+              data-bs-toggle="dropdown"
+              aria-expanded="false"
+            >
+              {/* Favorites {store.favorite.length} */}
+            </button>
+            <ul className="dropdown-menu">
+              {/* {store.favorite.map((item)=> {
+                return <li>
+                <a className="dropdown-item" href="#">
+                  {item.name}
+                </a>
+              </li>
+              })} */}
+            </ul>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+};
