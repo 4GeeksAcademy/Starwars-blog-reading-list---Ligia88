570ae166c5431a9c66ffbfd1218ea0713a607c53
Ligia88 <129026864+Ligia88@users.noreply.github.com>
2023-09-22 01:22:40 +0000

    image

--- src/js/views/Detail.jsx
+++ src/js/views/Detail.jsx
@@ -35,8 +35,8 @@ useEffect(()=>{
     <h1/> {search?.properties?.climate} <h1/>
     <h1/> {search?.properties?.terraine} <h1/>
     <h1/> {search?.properties?.population} <h1/>
-    <img src={"https://starwars-visualguide.com/assets/img/characters/"+search?.properties?.uid+".jpg"}  alt="Personajes" />
-    <img src={"https://starwars-visualguide.com/assets/img/planets/"+search?.properties?.uid+".jpg"}  alt="Planets" />
+    <img src={`https://starwars-visualguide.com/assets/img/characters/${search?.properties?.uid}.jpg`} alt="Personajes" />
+    <img src={`https://starwars-visualguide.com/assets/img/planets/${search?.properties?.uid}.jpg`} alt="Planets" />
     </>
 
 
